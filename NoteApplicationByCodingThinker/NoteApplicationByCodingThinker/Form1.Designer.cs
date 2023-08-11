1c248acf80bc6541e48e54e41961101828fe5905
Sukriye26 <cvdrskrye97@gmail.com>
2023-08-12 01:00:37 +0300

    deneme

---
+++ NoteApplicationByCodingThinker/NoteApplicationByCodingThinker/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace NoteApplicationByCodingThinker
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            titleTextBox = new TextBox();
+            noteTextBox = new TextBox();
+            editButton = new Button();
+            previousNotes = new DataGridView();
+            deleteButton = new Button();
+            newNoteButton = new Button();
+            saveButton = new Button();
+            titleLabel = new Label();
+            noteLabel = new Label();
+            previousNotesLabel = new Label();
+            ((System.ComponentModel.ISupportInitialize)previousNotes).BeginInit();
+            SuspendLayout();
+            // 
+            // titleTextBox
+            // 
+            titleTextBox.Location = new Point(374, 41);
+            titleTextBox.Name = "titleTextBox";
+            titleTextBox.PlaceholderText = "Please write the title here";
+            titleTextBox.Size = new Size(305, 23);
+            titleTextBox.TabIndex = 1;
+            // 
+            // noteTextBox
+            // 
+            noteTextBox.Location = new Point(374, 91);
+            noteTextBox.Multiline = true;
+            noteTextBox.Name = "noteTextBox";
+            noteTextBox.PlaceholderText = "Please write the note here";
+            noteTextBox.Size = new Size(305, 223);
+            noteTextBox.TabIndex = 2;
+            // 
+            // editButton
+            // 
+            editButton.BackColor = Color.LavenderBlush;
+            editButton.FlatStyle = FlatStyle.System;
+            editButton.Location = new Point(39, 324);
+            editButton.Name = "editButton";
+            editButton.Size = new Size(122, 23);
+            editButton.TabIndex = 3;
+            editButton.Text = "Edit";
+            editButton.UseVisualStyleBackColor = false;
+            editButton.Click += editButton_Click;
+            // 
+            // previousNotes
+            // 
+            previousNotes.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            previousNotes.BackgroundColor = SystemColors.Window;
+            previousNotes.BorderStyle = BorderStyle.Fixed3D;
+            previousNotes.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            previousNotes.Location = new Point(10, 41);
+            previousNotes.Name = "previousNotes";
+            previousNotes.RowTemplate.Height = 25;
+            previousNotes.Size = new Size(331, 273);
+            previousNotes.TabIndex = 4;
+            // 
+            // deleteButton
+            // 
+            deleteButton.BackColor = Color.LavenderBlush;
+            deleteButton.FlatStyle = FlatStyle.System;
+            deleteButton.Location = new Point(188, 324);
+            deleteButton.Name = "deleteButton";
+            deleteButton.Size = new Size(122, 23);
+            deleteButton.TabIndex = 4;
+            deleteButton.Text = "Delete";
+            deleteButton.UseVisualStyleBackColor = false;
+            deleteButton.Click += deleteButton_Click;
+            // 
+            // newNoteButton
+            // 
+            newNoteButton.BackColor = Color.LavenderBlush;
+            newNoteButton.FlatStyle = FlatStyle.System;
+            newNoteButton.Location = new Point(538, 324);
+            newNoteButton.Name = "newNoteButton";
+            newNoteButton.Size = new Size(122, 23);
+            newNoteButton.TabIndex = 6;
+            newNoteButton.Text = "New Note";
+            newNoteButton.UseVisualStyleBackColor = false;
+            newNoteButton.Click += newNoteButton_Click;
+            // 
+            // saveButton
+            // 
+            saveButton.BackColor = Color.LavenderBlush;
+            saveButton.FlatStyle = FlatStyle.System;
+            saveButton.Location = new Point(394, 324);
+            saveButton.Name = "saveButton";
+            saveButton.Size = new Size(122, 23);
+            saveButton.TabIndex = 5;
+            saveButton.Text = "Save";
+            saveButton.UseVisualStyleBackColor = false;
+            saveButton.Click += saveButton_Click;
+            // 
+            // titleLabel
+            // 
+            titleLabel.AutoSize = true;
+            titleLabel.Location = new Point(374, 23);
+            titleLabel.Name = "titleLabel";
+            titleLabel.Size = new Size(32, 15);
+            titleLabel.TabIndex = 7;
+            titleLabel.Text = "Title:";
+            // 
+            // noteLabel
+            // 
+            noteLabel.AutoSize = true;
+            noteLabel.Location = new Point(374, 73);
+            noteLabel.Name = "noteLabel";
+            noteLabel.Size = new Size(36, 15);
+            noteLabel.TabIndex = 8;
+            noteLabel.Text = "Note:";
+            // 
+            // previousNotesLabel
+            // 
+            previousNotesLabel.AutoSize = true;
+            previousNotesLabel.Location = new Point(10, 23);
+            previousNotesLabel.Name = "previousNotesLabel";
+            previousNotesLabel.Size = new Size(89, 15);
+            previousNotesLabel.TabIndex = 9;
+            previousNotesLabel.Text = "Previous Notes:";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(700, 381);
+            Controls.Add(previousNotesLabel);
+            Controls.Add(noteLabel);
+            Controls.Add(titleLabel);
+            Controls.Add(newNoteButton);
+            Controls.Add(saveButton);
+            Controls.Add(previousNotes);
+            Controls.Add(deleteButton);
+            Controls.Add(editButton);
+            Controls.Add(noteTextBox);
+            Controls.Add(titleTextBox);
+            Margin = new Padding(3, 2, 3, 2);
+            Name = "Form1";
+            Text = "DotNote";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)previousNotes).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox titleTextBox;
+        private TextBox noteTextBox;
+        private Button editButton;
+        private DataGridView previousNotes;
+        private Button deleteButton;
+        private Button newNoteButton;
+        private Button saveButton;
+        private Label titleLabel;
+        private Label noteLabel;
+        private Label previousNotesLabel;
+    }
+}
---
+++ NoteApplicationByCodingThinker/NoteApplicationByCodingThinker/Form1.cs
@@ -0,0 +1,96 @@
+using System.Data;
+
+namespace NoteApplicationByCodingThinker
+
+{
+    public partial class Form1 : Form
+    {
+        // neden data table diyebiirsiniz bu not uygulamasýný yapmakta epey geç kaldým puan alamayacak olsam da yapmam gerekiyor. 
+        // baþka kaynaða bakmayýn dediniz ama ben data table kullanmayý zaten biliyordum :) biz lisede c# dersi alýrken veritabanýndan önce data table kullanmýþtýk.
+        // dersi tekrar izlemeden yapýyorum bu arada sizin yaptýðýnýz bazý þeyleri koymamýþ olabilirim iþlemediðiniz yerleri eklemiþ olabilirim ama kiþiselleþtirmek bizim elimizdeydi.
+        // try catch bloðunu, buton click eventini ben icat etmedim ama bu kod iliðimle kemiðimle bana ait oldu, GURURLUYUM :) - ki ödevi anlamanýz lazým derken de bunu kastettiðniizi düþünüyorum.
+        // Eðer vakit bulabilirsem biþeyler daha deneyeceðim ama diðer ödevi yetiþtirmem gerekiyor. Belki yaratýcýlýðýmý orda kullanabilirim. 
+        // Zaten yazarken de farkettim keþke ödevi verdiðinizde 10 dakika ayýrsaymýþým :( 
+        // Þuan bizim uygulamada çalýþan servis mimarisini tam anlamadýðým için biraz kötü hissediyorum. Ýnþallah kurs bittiðinde üst satýrda yazdýðým gibi hissederim bu düþüncem için de. 
+
+        DataTable notes = new DataTable();
+        bool revise = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            this.BackColor = System.Drawing.ColorTranslator.FromHtml("#e8eaf6");
+            notes.Columns.Clear();             // tamamen alýþkanlýk :)
+            notes.Columns.Add("Title");        // kullanacaðým sütun isimlerini ekliyorum. 
+            notes.Columns.Add("Note");
+
+            //data table'ý yerine yerleþtirme vakti
+            previousNotes.DataSource = notes;
+
+
+        }
+
+        private void newNoteButton_Click(object sender, EventArgs e)
+        {
+            titleTextBox.Text = "";
+            noteTextBox.Text = "";
+
+            // titleTextBox.Clear(); ikisinin farký yok gibiydi ama bir ara bakacaðým buna
+            // javada hep text="" kullandýk alýþkanlýk sanýrým bu da
+
+
+        }
+
+        private void editButton_Click(object sender, EventArgs e)
+        {
+            //umarým editleme hayalimdeki gibi çalýþýr
+            titleTextBox.Text = notes.Rows[previousNotes.CurrentCell.RowIndex].ItemArray[0].ToString();
+            noteTextBox.Text = notes.Rows[previousNotes.CurrentCell.RowIndex].ItemArray[1].ToString();
+
+            revise = true; // bu notu biliyorum zaten kayýtlý güncelleyeceðim demeye çalýþýyorum
+
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            // acaba bildiðim üç beþ konuyu nasýl gösterebilirim diye düþünürken neden delete yapýp hata aldýrmayayým ki dedim ve ortaya aþaðýdaki blok çýktý
+
+            try
+            {
+                //buradaki mantýðý aslýnda derste lisbox'ýn selectedItem'ýný alýrken yaptýðýnýz mantýktan yola çýkarak kurdum
+                // currentCell'miþ o an seçili olan item buna dokümantasyondan baktým
+
+                notes.Rows[previousNotes.CurrentCell.RowIndex].Delete();
+
+            }catch (Exception ex) 
+            {
+                MessageBox.Show("Not a valid note"); // Kullanýcýya nullPointerException göstermeyelim tabi
+                Console.WriteLine(ex.Message);       // ama meraklýsýna hatayý da gösterebiliriz, neden olmasýn?
+            }
+
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            if (revise) // eðer bu notu biliyorsam
+            {
+                notes.Rows[previousNotes.CurrentCell.RowIndex]["Title"] = titleTextBox.Text; //  baþlýðý ve
+                notes.Rows[previousNotes.CurrentCell.RowIndex]["Note"] = noteTextBox.Text;   // notu data table'ýn seçili olan itemine gönder
+            }
+            else // notu tanýmýyorsam
+            {
+                notes.Rows.Add(titleTextBox.Text, noteTextBox.Text); // baþlýðý [0] ve notu [1] olan yeni bir öge ekle
+            }
+
+            // her ne olursa olsun iþ bitince temizlik 
+            titleTextBox.Text = "";
+            noteTextBox.Text = "";
+            revise = false;  // notla iþim bitti bir sonraki notlarý bilemem
+        }
+    }
+}
